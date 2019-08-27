4df353a1ab95a08f0b4fe367d2a0bbe887652ce5
Faith <wanjirufaith83@gmail.com>
2019-08-27 16:41:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form-helper> 
+        <div slot ="form-header">
+          <h3>Pension Form</h3>
+          <hr>
+          <p>Fill the form as required</p>
+       </div> 
+
+      <div slot="form-field">
+        <input type="text" placeholder="name" required/><br>
+        <input type ="password" placeholder = "password" required/>
+      </div>
+
+    <div slot="form-controls">
+      <button v-on:click="handleSubmit">Submit</button>
+    </div>
+    
+    </form-helper>
+  </div>
+</template>
+
+
+
+
+<script>
+import formHelper from './components/formHelper.vue' 
+export default{
+  components:{
+    'form-helper':formHelper
+  },
+  data(){
+    return{
+      title:"I like VueJs"
+
+    }
+  },
+  methods:{
+
+  }
+  }
+
+</script>
+
+<style lang="scss">
+
+
+</style>
---
+++ src/components/formHelper.vue
@@ -0,0 +1,84 @@
+<template>
+ <div>
+
+    <form>
+    <h1>hyhyhyhy</h1>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+
+      <div id="form-field">
+        <slot name="form-field"></slot>
+      </div> 
+
+      <div id="form-controls">
+        <slot name="form-controls"></slot>
+        </div>
+
+       <div id="useful-links">
+            <ul>
+                <li><a href='#'>Link 1</a></li>
+                <li><a href='#'>Link 2</a></li>
+                <li><a href='#'>Link 3</a></li>
+                <li><a href='#'>Link 4</a></li>
+            </ul>
+       </div>
+      </form>
+  </div>
+</template>
+<script>
+ 
+export default{
+  components:{
+    
+  },
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
+  }
+
+</script>
+
+<style scoped>
+h3{
+    background:green;
+    color: balck;
+    max-height: 600px;
+
+}
+#form-field{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+    display: inline;
+    float: none;
+    background:blue;
+
+}
+#useful-links{
+    color: red;
+  font-family: helvetica;
+  text-decoration: none;
+  text-transform: lowercase;
+
+}
+#form-controls{
+    display: flex;
+  align-items: center;
+  justify-content: center;
+ 
+
+}
+#form-controls button{
+    border-radius:3px;
+    color:blue;
+    background:red;
+}
+
+
+</style>
